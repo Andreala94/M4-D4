b458ae9cd6587a157a356cb1e50b51bd56fe2691
Andrea Lauro <125922974+Andreala94@users.noreply.github.com>
2023-05-26 21:17:36 +0200

    commit

--- javascript.js
+++ javascript.js
@@ -1,58 +1,49 @@
 let cerca = document.getElementById("cerca");
 // console.log(cerca);
-let libriContainer = document.getElementById("album-container");
-let card = document.getElementById("main")
 
+// let card = document.getElementById("main")
 
-
-// const getAlbum = () =>{
-
-//  fetch('https://striveschool-api.herokuapp.com/books')
-//   .then(response => response.json())
-//   .then(data => {
-    
-//     console.log(data);
-//   })
-//   .catch((err) =>{console.log("Hai un errore:" + err)});;
-
-// }
-
+let libri = document.getElementById("album-container");
 
 const getAlbum = () => {
-    return fetch("https://striveschool-api.herokuapp.com/books", {
-        
-    })
-        .then((res) => {
-            return res.json()
-    })
-        .then((res2) => {
-            albumCard(res2.photos)
-            console.log(res2)
+
+    fetch('https://striveschool-api.herokuapp.com/books')
+        .then(response => response.json())
+        .then(data => {
+
+            let row = document.createElement("div")
+            row.classList.add("row")
+            libri.appendChild(row)
+
+            data.forEach(element => {
+                let titolo = element.title;
+                let immagine = element.img;
+                let prezzo = element.price;
+                // console.log(titolo);
+                // console.log(immagine);
+                // console.log(prezzo);
+
+                let col = document.createElement("div")
+                col.classList.add("col-md-3")
+                let card = document.createElement("card")
+                card.classList.add("card")
+                let img = document.createElement("img");
+                img.src = element.src;
+                card.appendChild(img)
+                col.appendChild(card)
+                row.appendChild(col)
+
+
+            });
+            console.log(data);
         })
-        .catch((err) => {console.error("Hai un errore: " + err)});
+        .catch((err) => { console.log("Hai un errore:" + err) });;
+
 }
+
 getAlbum();
 
-const albumCard = (photos) => {
-    let row = document.createElement("div")
-    row.classList.add("row")
-    libriContainer.appendChild(row)
-   
-    
-    
-   photos.forEach(photo => {
-    let col = document.createElement("div")
-    col.classList.add("col-md-3")
-    let card = document.createElement("card")
-    card.classList.add("card")
 
-    let img = document.createElement("img");
-        img.src = photo.src.small;
-        card.appendChild(img)
-        col.appendChild(card)
-        row.appendChild(col)
-   });   
-    
 
-}
+
 
