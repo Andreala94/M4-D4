a83a580b1d651bbb82a826cf42872f500e1c6941
Andrea Lauro <125922974+Andreala94@users.noreply.github.com>
2023-05-27 10:43:26 +0200

    commit

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let cerca = document.getElementById("cerca");
 // console.log(cerca);
 
 // let card = document.getElementById("main")
-
+let carrello = document.getElementById("carrello")
 let libri = document.getElementById("album-container");
 
 const getAlbum = () => {
@@ -19,17 +19,39 @@ const getAlbum = () => {
                 let titolo = element.title;
                 let immagine = element.img;
                 let prezzo = element.price;
-                // console.log(titolo);
-                // console.log(immagine);
-                // console.log(prezzo);
-
+                
+                // Creo la card
                 let col = document.createElement("div")
-                col.classList.add("col-md-3")
+                col.classList.add("col-md-2")
+                col.classList.add("col-sm-6")
                 let card = document.createElement("card")
                 card.classList.add("card")
                 let img = document.createElement("img");
-                img.src = element.src;
+                img.src = immagine;
+                
                 card.appendChild(img)
+
+                // Aggiungi un paragrafo
+                let paragrafo = document.createElement("p");
+                paragrafo.textContent = titolo;
+                card.appendChild(paragrafo);
+
+                // Aggiungi un bottone
+                let bottone = document.createElement("button");
+
+                bottone.textContent = "Acquista";
+                bottone.classList.add("bg-primary");
+                bottone.classList.add("rounded-3");
+                card.appendChild(bottone);
+                // evento per mettere il bordo colorato
+                bottone.addEventListener("click", () => {
+                    carrello.innerHTML += (col.outerHTML)
+                   card.classList.add("border-danger")
+                   card.classList.add("border-3")
+
+                });
+
+
                 col.appendChild(card)
                 row.appendChild(col)
 
@@ -42,6 +64,7 @@ const getAlbum = () => {
 }
 
 getAlbum();
+
 
 
 
